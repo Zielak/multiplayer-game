fcbf786db18c83dec0c5a3a09467cda11de77ef8
Darek Greenly <git@zielak.pl>
2017-09-19 20:53:29 +0200

    clients finally get all the info they need.

--- rooms/cardsGame/base.js
+++ rooms/cardsGame/base.js
@@ -2,9 +2,14 @@ const uuid = require('uuid/v4')
 const Player = require('./player')
 const utils = require('./utils')
 
+const objects = new Map()
+
 module.exports = class Base {
   constructor(options = {}){
     this.id = uuid()
+
+    // Store a reference to itself by ID
+    objects.set(this.id, this)
 
     // Real-life size (in CM) and position
     this.x = utils.default(options.x, 0)
@@ -13,7 +18,7 @@ module.exports = class Base {
     this.width = utils.default(options.width, 5)
     
     // Parent element which holds this container
-    this.parent = utils.default(options.parent, null)
+    this.parent = options.parent ? utils.default(options.parent.id, null) : null
   }
 
   /**
@@ -23,12 +28,23 @@ module.exports = class Base {
    * @return {Player|null} `Player` or `null` if this container doesn't belong to anyone
    */
   get owner(){
-    if(this.parent instanceof Player){
+    if(Base.get(this.parent) instanceof Player){
       return this.parent
     } else if (this.parent === null) {
       return null
     } else {
-      return this.parent.owner
+      return Base.get(this.parent.owner)
     }
+  }
+
+  /**
+   * Get a reference to the object by its ID
+   * 
+   * @static
+   * @param {string} id 
+   * @returns {any}
+   */
+  static get(id){
+    return objects.get(id)
   }
 }
--- rooms/cardsGame/presets.js
+++ rooms/cardsGame/presets.js
@@ -1,23 +1,20 @@
 const ClassicCard = require('./classicCard')
-const Deck = require('./containers/deck')
 
 module.exports = {
-  classicCardsDeck: () => {
+  classicCards: () => {
     const ranks = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
     const suits = ['H','S','C','D']
 
-    const deck = new Deck({
-      elements: suits.reduce((prevS, suit) => [
-        ...prevS,
-        ...ranks.reduce((prevR, rank) => [
-          ...prevR,
-          new ClassicCard({suit, rank})
-        ], [])
+    const cards = suits.reduce((prevS, suit) => [
+      ...prevS,
+      ...ranks.reduce((prevR, rank) => [
+        ...prevR,
+        new ClassicCard({suit, rank})
       ], [])
-    })
+    ], [])
 
-    console.info(`   CardAPI, presets: created a deck of ${deck.length} cards`)
+    console.info(`   CardAPI, presets: created a deck of ${cards.length} cards`)
 
-    return deck
+    return cards
   }
 }
