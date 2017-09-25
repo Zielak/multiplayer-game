7d44516dca8ff62c878519d583265f52659bdec7
Darek Greenly <git@zielak.pl>
2017-09-25 22:15:49 +0200

    I did some rendering, but client doesn't get store any containers nor cards

--- client/cardsGame/table/table.js
+++ client/cardsGame/table/table.js
@@ -5,10 +5,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-// import ClassicCard from '../card/classicCard'
-// import {getElementById, getAllParents} from '../utils'
+import ClassicCard from '../card/classicCard'
+import {
+  // getElementById,
+  getAllParents,
+  translatePoint,
+  rotatePoint,
+} from '../utils'
 
 import Player from '../player/player'
+import Deck from '../containers/deck/deck'
+import Pile from '../containers/pile/pile'
+// import Hand from '../containers/hand/hand'
 
 import './table.scss'
 
@@ -33,26 +41,28 @@ class Table extends React.Component {
   render() {
     let players = []
 
-    if(this.props.players && this.props.players.list){
+    if (this.props.players && this.props.players.list) {
       const angle = this.props.players.list ? 360 / this.props.players.list.length : 0
       // TODO: align angle to the current player
       const startingAngle = 90
 
-      // TODO: Get all parents
-
-      // TODO: Transform this element regarding all parents transforms
-
-      players = this.props.players.list.map((player, idx) => ({
-        ...player,
-        angle: angle * idx + startingAngle,
-        idx,
-      })).map((element, idx) => {
+      players = this.props.players.list.map((player, idx) => {
+        // Get initial correct position on the table, and add an angle
         const position = positionFromAngle(angle * idx + 90, 40)
+        return {
+          ...player,
+          x: position.x,
+          y: position.y,
+          angle: angle * idx + startingAngle,
+          idx,
+        }
+      }).map((element) => {
+
         return (
-          <Player key={'player'+element.idx}
+          <Player key={'player' + element.idx}
             {...element}
-            x={element.dimensions.x + position.x}
-            y={element.dimensions.y + position.y}
+            x={element.dimensions.x}
+            y={element.dimensions.y}
 
             width={element.dimensions.width}
             height={element.dimensions.height}
@@ -61,13 +71,54 @@ class Table extends React.Component {
       })
     }
 
-    // const elements = [
-    //   ...this.props.containers,
-    //   ...this.props.cards,
-    // ].map(element => {
-    //   // Find parent
-    //   const owner = findOwner(elements, element)
-    // })
+    // Parse all current elements from props
+    // Returns renderable html elements
+    const elements = [
+      ...this.props.players && this.props.players.list || [],
+      ...this.props.containers || [],
+      ...this.props.cards || [],
+    ].map(element => {
+      // TODO: Get all parents
+      const parents = getAllParents(elements, element)
+      
+      // TODO: Transform this element regarding all parents transforms
+      const position = parents.reduce((point, parent) => {
+        const parentPos = {
+          x: parent.dimensions.x,
+          y: parent.dimensions.y,
+        }
+        let p = translatePoint(point, parentPos)
+        p = rotatePoint(p, parentPos, parent.dimensions.angle)
+        return p
+      }, {x: element.dimensions.x, y: element.dimensions.y})
+      
+      return {
+        ...element,
+        x: position.x,
+        y: position.y,
+      }
+    }).map(element => {
+      if(element.type === 'deck'){
+        return (
+          <Deck key={'deck' + element.idx}
+            {...element}
+          ></Deck>
+        )
+      } else if(element.type === 'pile'){
+        return (
+          <Pile key={'pile' + element.idx}
+            {...element}
+          ></Pile>
+        )
+      } else if(element.type === 'card') {
+        return (
+          <ClassicCard key={'card' + element.idx}
+            {...element}
+          ></ClassicCard>
+        )
+      }
+      
+    })
 
     return (
       <div className='Table'>
--- client/cardsGame/utils.js
+++ client/cardsGame/utils.js
@@ -3,14 +3,34 @@ export const getElementById = (everything, id) => everything.find(el => el.id ==
 
 // TODO: test it
 export const getAllParents = (everything, element) => {
-  if(element.parent) {
+  if (element.parent) {
     const found = getElementById(everything, element.parent)
-    if(found){
+    if (found) {
       return [...getAllParents(everything, found)]
     } else {
       return []
     }
   } else {
     return []
+  }
+}
+
+export const translatePoint = (point, x, y) => {
+  return {
+    x: point.x + x,
+    y: point.y + y,
+  }
+}
+
+export const rotatePoint = (point, origin, angle) => {
+  const x1 = point.x - origin.x
+  const y1 = point.y - origin.y
+
+  const x2 = x1 * Math.cos(angle) - y1 * Math.sin(angle)
+  const y2 = x1 * Math.sin(angle) + y1 * Math.cos(angle)
+
+  return {
+    x: x2 + origin.x,
+    y: y2 + origin.y,
   }
 }
