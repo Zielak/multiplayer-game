15c05006b25666963895e431f080771c4cec012e
Darek Greenly <git@zielak.pl>
2017-10-13 21:14:02 +0200

    card styles

--- client/cardsGame/card/classicCard.js
+++ client/cardsGame/card/classicCard.js
@@ -6,23 +6,9 @@ require('./classicCard.scss')
 class ClassicCard extends React.Component {
 
   render() {
-    const face = this.props.faceUp ?
-      <div className="face">
-        <div className="">
-          {this.renderFrontGraphics(this.props.suit, this.props.rank)}
-        </div>
-        <div className="icons">
-          <div className="rank">{this.renderRank(this.props.rank)}</div>
-          <div className="suit">{this.renderSuit(this.props.suit)}</div>
-        </div>
-        <div className="icons reverse">
-          <div className="rank">{this.renderRank(this.props.rank)}</div>
-          <div className="suit">{this.renderSuit(this.props.suit)}</div>
-        </div>
-      </div> :
-      <div className="back">
-        {this.renderBackGraphics()}
-      </div>
+    const render = this.props.faceUp ?
+      this.renderFrontGraphics(this.props.suit, this.props.rank) :
+      this.renderBackGraphics()
 
     return (
       <div className="ClassicCard" style={{
@@ -30,17 +16,28 @@ class ClassicCard extends React.Component {
         top: this.props.y + 50 + '%',
         '--angle': (this.props.angle || 0) + 'deg',
       }}>
-        {face}
+        {render}
       </div>
     )
   }
 
-  renderFrontGraphics(/*suit, rank*/) {
-    return 'card'
+  renderFrontGraphics(suit, rank) {
+    return <div className="front">
+      <div className="icons">
+        <div className="rank">{this.renderRank(rank)}</div>
+        <div className="suit">{this.renderSuit(suit)}</div>
+      </div>
+      <div className="icons reverse">
+        <div className="rank">{this.renderRank(rank)}</div>
+        <div className="suit">{this.renderSuit(suit)}</div>
+      </div>
+    </div>
   }
 
   renderBackGraphics() {
-    return 'bg'
+    return <div className="back">
+      card
+    </div>
   }
 
   renderRank(rank) {
