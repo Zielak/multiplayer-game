1b4671c6241ed0f296996b19f373e6d97118335c
Darek Greenly <git@zielak.pl>
2017-11-14 19:41:44 +0100

    cleanup: remove renderer

--- client/cardsGame/index.js
+++ client/cardsGame/index.js
@@ -1,4 +1,3 @@
-export { default as Renderer } from './renderer'
 export { default as Deck } from './containers/deck/deck'
 export { default as Pile } from './containers/pile/pile'
 export { default as Row } from './containers/row/row'
--- client/cardsGame/player/player.js
+++ client/cardsGame/player/player.js
@@ -1,7 +1,6 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-// import { worldTransformCSS } from '../renderer'
 
 import './player.scss'
 
