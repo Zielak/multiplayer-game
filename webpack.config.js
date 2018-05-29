b09f510d6eea2cb161146f5f6083470e5b0e240e
Darek Zieliński <dariusz.zielinski@performgroup.com>
2018-05-29 11:30:29 +0200

    update webpack config + webpack-cli plz

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,47 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
 
-const main = {
-  entry: './client/index.js',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loaders: ['babel-loader'],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2,
-              minimize: false,
+module.exports = env => {
+  return {
+    entry: './client/index.js',
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    mode: env === 'production' ? 'production' : 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist',
+      hot: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loaders: ['babel-loader'],
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 2,
+                minimize: false,
+              },
             },
-          },
-          'sass-loader',
-        ]
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'client/index.ejs'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+            'sass-loader',
+          ]
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'client/index.ejs'
+      }),
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  }
 }
-
-module.exports = main
